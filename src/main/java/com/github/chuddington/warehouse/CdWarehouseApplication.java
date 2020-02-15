1ccd8d591901679baf7211f233e5ede898d8e4aa
Chuddington <michaelchadwick94@gmail.com>
2020-02-15 13:52:30 +0000

    Initial Commit

---
+++ src/main/java/com/github/chuddington/warehouse/CdWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.github.chuddington.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdWarehouseApplication.class, args);
+	}
+
+}
